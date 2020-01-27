7688e1958cb4e245968c75592a2e4ba05630270c
cyk <33898610+hitcyk@users.noreply.github.com>
2020-01-27 21:08:19 +0800

    update the file on 1.27
    delete the where clause

--- test.sql
+++ test.sql
@@ -4,10 +4,9 @@ from table1 as t1
 left join table2 as t2 
 on t1.individual_id = t2.individual_id
 where 1=1
-and age >= 69
 ;
 
 # don't run this, because this is just a bullshit
 # this is true cyk write!
-
+# add a new comment to show cyk how to use github
 

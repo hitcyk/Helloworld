8f23bd0348067cf1a2fae663d70d74cc08684490
cyk <33898610+hitcyk@users.noreply.github.com>
2020-01-27 20:02:38 +0800

    edit this file to test
    add 1=1 in line 6

--- test.sql
+++ test.sql
@@ -3,7 +3,10 @@ select distinct t1.individual_id, t2.birth_date
 from table1 as t1 
 left join table2 as t2 
 on t1.individual_id = t2.individual_id
-where age >= 69
+where 1=1
+and age >= 69
 ;
 
 # don't run this, because this is just a bullshit
+
+
